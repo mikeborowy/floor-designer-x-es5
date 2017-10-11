afb860705319f7af7721ff6a43a5e88ba493dd33
mikeborowy <mikeborowy@gmail.com>
2017-10-11 22:22:24 +0200

    Added dragActionc enum

---
+++ FloorDesignerEx.Client.React/app/components/designerScreen/common/dragActions.js
@@ -0,0 +1,14 @@
+﻿export default DragActions = () => {
+
+    let dragActions = {}
+    dragActions.NONE = "none";
+    dragActions.SELECTED = "selected";
+    dragActions.UNSELECTED = "unselected";
+    dragActions.CREATED = "created";
+    dragActions.DRAG = "drag";
+    dragActions.RESIZE = "resize";
+    dragActions.ROTATE = "rotate";
+    dragActions.DELETE = "delete";
+
+    return (dragActions)
+}
