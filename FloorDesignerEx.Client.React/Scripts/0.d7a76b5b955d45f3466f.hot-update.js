6b1f8f1c6bd87097d142b1737d9ef5b301d41c79
mikeborowy <mikeborowy@gmail.com>
2017-11-12 13:24:25 +0100

    Deleted folders from root

---
+++ FloorDesignerEx.Client.React/Scripts/0.d7a76b5b955d45f3466f.hot-update.js
@@ -0,0 +1,63 @@
+webpackHotUpdate(0,{
+
+/***/ 208:
+/*!***********************************************************!*\
+  !*** ./app/components/designerScreen/common/shapesCfg.js ***!
+  \***********************************************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(module) {/* REACT HOT LOADER */ if (true) { (function () { var ReactHotAPI = __webpack_require__(/*! ./~/react-hot-api/modules/index.js */ 8), RootInstanceProvider = __webpack_require__(/*! ./~/react-hot-loader/RootInstanceProvider.js */ 9), ReactMount = __webpack_require__(/*! react-dom/lib/ReactMount */ 6), React = __webpack_require__(/*! react */ 2); module.makeHot = module.hot.data ? module.hot.data.makeHot : ReactHotAPI(function () { return RootInstanceProvider.getRootInstances(ReactMount); }, React); })(); } try { (function () {
+	
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	
+	var _shapesGFX = __webpack_require__(/*! ../../../assets/shapes/shapesGFX */ 88);
+	
+	var shapesList = function shapesList() {
+	    return [{
+	        id: '1',
+	        name: 'shape-room-sqr-2x2',
+	        image: _shapesGFX.shapesGFX.sq_2x2,
+	        width: 2,
+	        height: 2,
+	        desc: 'Square Room',
+	        isDraggable: true
+	    }, {
+	        id: '2',
+	        name: 'shape-room-sqr-3x3',
+	        image: _shapesGFX.shapesGFX.sq_3x3,
+	        width: 3,
+	        height: 3,
+	        desc: 'Square Room',
+	        isDraggable: true
+	    }, {
+	        id: '3',
+	        name: 'shape-room-rct-3x2',
+	        image: _shapesGFX.shapesGFX.rq_3x2,
+	        width: 3,
+	        height: 2,
+	        desc: 'Rectangle Room',
+	        isDraggable: true
+	    }, {
+	        id: '4',
+	        name: 'shape-room-l-3x2',
+	        image: _shapesGFX.shapesGFX.ls_3x3,
+	        width: 3,
+	        height: 3,
+	        desc: 'L-Shape Room',
+	        isDraggable: true
+	    }];
+	};
+	
+	exports.default = shapesList;
+	
+	/* REACT HOT LOADER */ }).call(this); } finally { if (true) { (function () { var foundReactClasses = module.hot.data && module.hot.data.foundReactClasses || false; if (module.exports && module.makeHot) { var makeExportsHot = __webpack_require__(/*! ./~/react-hot-loader/makeExportsHot.js */ 10); if (makeExportsHot(module, __webpack_require__(/*! react */ 2))) { foundReactClasses = true; } var shouldAcceptModule = true && foundReactClasses; if (shouldAcceptModule) { module.hot.accept(function (err) { if (err) { console.error("Cannot apply hot update to " + "shapesCfg.js" + ": " + err.message); } }); } } module.hot.dispose(function (data) { data.makeHot = module.makeHot; data.foundReactClasses = foundReactClasses; }); })(); } }
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(/*! ./../../../../~/webpack/buildin/module.js */ 11)(module)))
+
+/***/ }
+
+})
+//# sourceMappingURL=0.d7a76b5b955d45f3466f.hot-update.js.map
